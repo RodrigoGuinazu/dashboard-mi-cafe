c8be96b718e359c53c849eba10a2b795af635f84
analiavargas <vargas_va@live.com.ar>
2021-04-10 20:16:50 -0300

    Se setearon los encabezados del listado detallado de productos en DB

--- src/components/ProductsTable/TableBody.js
+++ src/components/ProductsTable/TableBody.js
@@ -4,24 +4,10 @@ function TableBody(props) {
     return (
         <>
             <tr>
-                <td>Tiger Nixon</td>
-                <td>System Architect</td>
-                <td>$320,800</td>
-                <td>
-                    <ul>
-                        <li>Category 01</li>
-                        <li>Category 02</li>
-                        <li>Category 03</li>
-                    </ul>
-                </td>
-                <td>
-                    <ul>
-                        <li><span className="text-danger">Red</span></li>
-                        <li><span className="text-primary">Blue</span></li>
-                        <li><span className="text-success">Green</span></li>
-                    </ul>
-                </td>
-                <td>245</td>
+                <td>{props.title}</td>
+                <td>{props.description}</td>
+                <td>{props.price}</td>
+                <td>{props.category}</td>
             </tr> 
         </>
     )
