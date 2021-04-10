dfb279e891532556293002c7c1f19ed612b7fa7a
analiavargas <vargas_va@live.com.ar>
2021-04-10 20:20:13 -0300

    Seteo de título de tabla

--- src/components/ProductsTable/index.js
+++ src/components/ProductsTable/index.js
@@ -4,7 +4,7 @@ import Table from './Table'
 function ProductsTable() {
     return (
         <>
-            <h1 className="h3 mb-2 text-gray-800">All the products in the Database</h1>
+            <h1 className="h3 mb-2 text-gray-800">Lista general de productos en Base de Datos</h1>
             <div className="card shadow mb-4">
                 <div className="card-body">
                     <Table />
