d8e49bd5a908d9482d1a77c8bcada3c22c0389a2
analiavargas <vargas_va@live.com.ar>
2021-04-10 20:14:20 -0300

    Se seteó la presentación de total de productos por categoría

--- src/components/Body/CategoryCard.js
+++ src/components/Body/CategoryCard.js
@@ -9,7 +9,7 @@ function CategoryCard(props) {
             <div className="col-lg-6 mb-4">
                 <div className="card bg-info text-white shadow">
                     <div className="card-body">       
-                        {props.title}
+                        {props.title}: {props.count} productos
                     </div>
                 </div>
             </div>          
