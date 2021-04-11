ea5adaf6ed2114d3c2bd6ef99ee669ae02b55527
RowaSanchez <rodrigosan06@gmail.com>
2021-04-11 20:31:54 -0300

    se modificó la tabla de productos en el dashboard

--- src/components/ProductsTable/index.js
+++ src/components/ProductsTable/index.js
@@ -4,12 +4,14 @@ import Table from './Table'
 function ProductsTable() {
     return (
         <>
+            <a id="tabla-productos">
             <h1 className="h3 mb-2 text-gray-800">Productos en la Base de Datos</h1>
             <div className="card shadow mb-4">
                 <div className="card-body">
                     <Table />
                 </div>
             </div>
+            </a>
         </>
     )
 }
