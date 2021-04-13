d18e73cc68331fd5979e3a15bfa316d2b05a7e22
analiavargas <vargas_va@live.com.ar>
2021-04-12 21:41:42 -0300

    Se seteo el link del ancla

--- src/components/Sidebar/MenuTitles.js
+++ src/components/Sidebar/MenuTitles.js
@@ -4,7 +4,7 @@ function MenuTitles(props) {
     return (
         <>
             <li className={props.clase}>
-                <a className="nav-link" href="/">
+                <a className="nav-link" href={props.link}>
                     <a style={{textDecoration: 'none', color: 'black'}} href={props.link}>
                         <i className={props.icon}></i>
                         <span>{props.title}</span>
